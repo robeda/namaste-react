ad633a1921dca28b6c04c7708390417012746623
Robeda <robeda27july@gmail.com>
2026-09-22 23:16:16 +0530

    Edpisode -3

--- App.js
+++ App.js
@@ -1,7 +1,52 @@
- const heading = React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Hello world from React!"),React.createElement("h2",{},"I am sibling of h1!")]),
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Hello world from React!"),React.createElement("h2",{},"I am sibling of h1!")])]);
- const createRoot = ReactDOM.createRoot(document.getElementById("root"))
- createRoot.render(heading)
+ import React from 'react';
+import { createRoot } from "react-dom/client";
+import logo from "./assets/logo2.png";
+/*
+header
+  logo
+  navItems
+Body
+  search
+  restaurant card
+footer
+   copyright
+   links
+   address
+*/
+const Header = () => {
+    return (
+    <div className="header">
+        <div >
+           <img
+        src="/assets/logo2.png"
+        alt="Logo"
+        style={{
+          width: "200px",
+          height: "200px",
+          objectFit: "contain"
+        }}
+        />
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+
+    </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+
+        </div>
+    )
+
+}
+ const root = createRoot(document.getElementById("root"));
+ root.render(<AppLayout/>);
